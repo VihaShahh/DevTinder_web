b8d79bdce125a1ff95b799fdfc4ee140b4769d52
VihaShahh <vihashah1103@gmail.com>
2025-12-31 01:12:44 +0530

    setup and hello world

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
-
   return (
-    <>
-      <h1> Hello World</h1>
-    </>
-
+    <div className="p-10">
+      <h1 className="text-3xl font-bold underline text-center text-pink-800">
+        Hello wuorld!
+      </h1>
+    </div>
   );
 }
 
-export default App
+export default App;
